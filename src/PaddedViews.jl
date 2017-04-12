d6080deace72382a1a9db5ef0c042ade30f122f5
Tim Holy <tim.holy@gmail.com>
2017-04-12 10:56:20 -0500

    Initial working commit

--- src/PaddedViews.jl
+++ src/PaddedViews.jl
@@ -1,5 +1,43 @@
+__precompile__(true)
+
 module PaddedViews
 
-# package code goes here
+export PaddedView
+
+immutable PaddedView{T,N,I,A} <: AbstractArray{T,N}
+    fillvalue::T
+    data::A
+    indices::I
+
+    function (::Type{PaddedView{T,N,I,A}}){T,N,I,A}(fillvalue::T,
+                                                    data::AbstractArray{T,N},
+                                                    indices::NTuple{N,AbstractUnitRange})
+        new{T,N,I,A}(fillvalue, data, indices)
+    end
+end
+(::Type{PaddedView}){T,N}(fillvalue, data::AbstractArray{T,N}, indices) =
+    PaddedView{T,N,typeof(indices),typeof(data)}(convert(T, fillvalue), data, indices)
+function (::Type{PaddedView}){T,N}(fillvalue, data::AbstractArray{T,N}, sz::Tuple{Integer,Vararg{Integer}})
+    inds = map(Base.OneTo, sz)
+    PaddedView{T,N,typeof(inds),typeof(data)}(convert(T, fillvalue), data, inds)
+end
+
+Base.indices(A::PaddedView) = A.indices
+@inline Base.indices(A::PaddedView, d::Integer) = d <= ndims(A) ? A.indices[d] : default_indices(A.indices)
+default_indices{N,I<:AbstractUnitRange}(::NTuple{N,I}) = convert(I, Base.OneTo(1))
+default_indices(::Any) = Base.OneTo(1)
+
+Base.size(A::PaddedView) = _size(A, indices(A))
+_size{N}(A, inds::NTuple{N,Base.OneTo}) = map(length, inds)
+_size(A, inds) = errmsg(A)
+errmsg(A) = error("size not supported for arrays with indices $(indices(A)); see http://docs.julialang.org/en/latest/devdocs/offset-arrays/")
+
+@inline function Base.getindex{T,N}(A::PaddedView{T,N}, i::Vararg{Int,N})
+    @boundscheck checkbounds(A, i...)
+    if Base.checkbounds(Bool, A.data, i...)
+        return A.data[i...]
+    end
+    return A.fillvalue
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,44 @@
-using PaddedViews
+using OffsetArrays
 using Base.Test
+ambs = detect_ambiguities(Base, Core)  # in case these have ambiguities of their own
+using PaddedViews
+@test isempty(setdiff(detect_ambiguities(PaddedViews, Base, Core), ambs))
 
-# write your own tests here
-@test 1 == 2
+# Basics
+for n = 0:5
+    a = PaddedView(0, ones(Int,ntuple(d->1,n)), ntuple(x->x+1,n))
+    @test indices(a) == ntuple(x->1:x+1,n)
+    @test a[1] == 1
+    n > 0 && @test a[2] == 0
+end
+a0 = reshape([3])
+a = PaddedView(-1, a0, ())
+@test indices(a) == ()
+@test ndims(a) == 0
+@test a[] == 3
+
+a = reshape(1:9, 3, 3)
+A = PaddedViews.PaddedView(0.0, a, (Base.OneTo(4), Base.OneTo(5)))
+@test eltype(A) == Int
+@test ndims(A) == 2
+@test size(A) === (4,5)
+@test indices(A) === (Base.OneTo(4), Base.OneTo(5))
+@test indices(A, 3) === Base.OneTo(1)
+@test A == [1 4 7 0 0;
+            2 5 8 0 0;
+            3 6 9 0 0;
+            0 0 0 0 0]
+
+A = PaddedViews.PaddedView(0.0, a, (0:4, -1:5))
+@test eltype(A) == Int
+@test ndims(A) == 2
+@test_throws ErrorException size(A)
+@test indices(A) === (0:4, -1:5)
+@test indices(A, 3) === 1:1
+@test A == OffsetArray([0 0 0 0 0 0 0;
+                        0 0 1 4 7 0 0;
+                        0 0 2 5 8 0 0;
+                        0 0 3 6 9 0 0;
+                        0 0 0 0 0 0 0], 0:4, -1:5)
+
+nothing
