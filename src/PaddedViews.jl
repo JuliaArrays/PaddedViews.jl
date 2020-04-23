cb776688fcb71e5af94d01f9f66c1736f7f4475d
Johnny Chen <johnnychen94@hotmail.com>
2020-04-23 10:11:07 +0800

    julia 0.7/1.0 ambiguity patch for `all`

--- src/PaddedViews.jl
+++ src/PaddedViews.jl
@@ -313,5 +313,10 @@ end
 # TODO use use short-circuit evaluation,
 Base.any(f::Function, A::PaddedView) = f(A.fillvalue) | any(f, parent(A))
 Base.all(f::Function, A::PaddedView) = f(A.fillvalue) & all(f, parent(A))
+if v"0.7" <= VERSION < v"1.1"
+    # ambiguity patch
+    # https://github.com/JuliaLang/julia/pull/30904
+    Base.all(::typeof(isinteger), ::PaddedView{<:Integer}) = true
+end
 
 end # module
