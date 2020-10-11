0038c9ae828592a6bbb7634d642018c886720d51
Johnny Chen <johnnychen94@hotmail.com>
2020-10-11 18:16:10 +0800

    remove result type annotation

--- src/PaddedViews.jl
+++ src/PaddedViews.jl
@@ -160,7 +160,7 @@ Base.size(A::PaddedView) = map(length, axes(A))
 
 Base.parent(A::PaddedView) = A.data
 
-Base.@propagate_inbounds function Base.getindex(A::PaddedView{T,N}, i::Vararg{Int,N})::T where {T,N}
+Base.@propagate_inbounds function Base.getindex(A::PaddedView{T,N}, i::Vararg{Int,N}) where {T,N}
     @boundscheck checkbounds(A, i...)
     if Base.checkbounds(Bool, A.data, i...)
         return convert(T, A.data[i...])
