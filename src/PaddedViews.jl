2a2e3751fa8965b1787648440fe62edab003e802
Ashwani Rathee <ab669522@gmail.com>
2021-05-26 20:12:16 +0530

    Clean Up

--- src/PaddedViews.jl
+++ src/PaddedViews.jl
@@ -10,39 +10,50 @@ export PaddedView, paddedviews, sym_paddedviews
     datapadded = PaddedView(fillvalue, data, sz)
     datapadded = PaddedView(fillvalue, data, sz, first_datum)
     datapadded = PaddedView{T}(args...)
+
 Create a padded version of the array `data`, where any elements within
 the span of `padded_axes` not assigned in `data` will have value
 `fillvalue`.
+
 Supply `data_axes` to specify an alterate set of axes for `data`, effectively
 relocating `data` to a different set of indices.
 This is shorthand for
+
     offsetdata = OffsetArray(data, data_axes)
     datapadded = PaddedView(fillvalue, offsetdata, padded_axes)
+
 using the [OffsetArrays](https://github.com/JuliaArrays/OffsetArrays.jl) package.
+
 Alternately, the padded array size `sz` can be specified, in which case `datapadded`
 starts indexing at 1.
 One may optionally specify the location of the `[1, 1, ...]` element of `data` with
 `first_datum`.
 Specifically, `datapadded[first_datum...]` corresponds to `data[1, 1, ...]`.
 `first_datum` defaults to all-1s.
+
 The view eltype `T` is optional. If not specified, then in most cases, `T` is inferred to be
 `eltype(data)`. In cases when `fillvalue` can't be converted to `eltype(data)`, `T` will be
 promoted the one that does. For example, when `fillvalue == nothing` and `eltype(data) == Float32`,
 the inferred eltype `T` will be `Union{Nothing, Float32}`.
+
 # Example
+
 ```jldoctest
 julia> using PaddedViews
+
 julia> a = collect(reshape(1:9, 3, 3))
 3×3 $(Array{Int,2}):
  1  4  7
  2  5  8
  3  6  9
+
 julia> PaddedView(-1, a, (4, 5))
 4×5 PaddedView(-1, ::$(Array{Int,2}), (Base.OneTo(4), Base.OneTo(5))) with eltype $Int:
   1   4   7  -1  -1
   2   5   8  -1  -1
   3   6   9  -1  -1
  -1  -1  -1  -1  -1
+
 julia> PaddedView(-1, a, (1:5,1:5), (2:4,2:4))
 5×5 PaddedView(-1, OffsetArray(::$(Array{Int,2}), 2:4, 2:4), (1:5, 1:5)) with eltype $Int with indices 1:5×1:5:
  -1  -1  -1  -1  -1
@@ -50,6 +61,7 @@ julia> PaddedView(-1, a, (1:5,1:5), (2:4,2:4))
  -1   2   5   8  -1
  -1   3   6   9  -1
  -1  -1  -1  -1  -1
+
 julia> PaddedView(-1, a, (0:4, 0:4))
 5×5 PaddedView(-1, ::$(Array{Int,2}), (0:4, 0:4)) with eltype $Int with indices 0:4×0:4:
  -1  -1  -1  -1  -1
@@ -57,6 +69,7 @@ julia> PaddedView(-1, a, (0:4, 0:4))
  -1   2   5   8  -1
  -1   3   6   9  -1
  -1  -1  -1  -1  -1
+
 julia> PaddedView(-1, a, (5,5), (2,2))
 5×5 PaddedView(-1, OffsetArray(::$(Array{Int,2}), 2:4, 2:4), (Base.OneTo(5), Base.OneTo(5))) with eltype $Int:
  -1  -1  -1  -1  -1
@@ -165,35 +178,45 @@ end
 
 """
     Aspad = paddedviews(fillvalue, A1, A2, ....)
+
 Pad the arrays `A1`, `A2`, ..., to a common size or set of axes,
 chosen as the span of axes enclosing all of the input arrays.
+
 The padding is applied to one direction. For example, values are filled to bottom-right part
 of the new array in two-dimensional case. Use [`sym_paddedviews`](@ref) if _both_ directions
 need to be padded.
+
 The axes of original array `A` will be preserved in the padded result `Ap`, hence it's true
 that `Ap[CartesianIndices(A)] == A`.
+
 # Example:
 ```jldoctest
 julia> using PaddedViews
+
 julia> a1 = reshape([1, 2, 3], 3, 1)
 3×1 $(Array{Int,2}):
  1
  2
  3
+
 julia> a2 = [4 5 6]
 1×3 $(Array{Int,2}):
  4  5  6
+
 julia> a1p, a2p = paddedviews(-1, a1, a2);
+
 julia> a1p
 3×3 PaddedView(-1, ::$(Array{Int,2}), (Base.OneTo(3), Base.OneTo(3))) with eltype $Int:
  1  -1  -1
  2  -1  -1
  3  -1  -1
+
 julia> a2p
 3×3 PaddedView(-1, ::$(Array{Int,2}), (Base.OneTo(3), Base.OneTo(3))) with eltype $Int:
   4   5   6
  -1  -1  -1
  -1  -1  -1
+
 julia> a1p[CartesianIndices(a1)]
 3×1 $(Array{Int,2}):
  1
@@ -219,6 +242,7 @@ function _extended_axes(A, inds, dims::UnitRange{Int64})
     end
     return inds
 end
+
 # Zero, one, and two arrays are common, improve inferrability
 paddedviews(fillvalue) = ()
 paddedviews(fillvalue, A1::AbstractArray) = (PaddedView(fillvalue, A1, outerinds(A1)),)
@@ -255,33 +279,43 @@ padrange(i1::AbstractRange, i2::AbstractRange) = padrange(convert(UnitRange{Int}
 
 """
     Aspad = sym_paddedviews(fillvalue, A1, A2, ....)
+
 Pad the arrays `A1`, `A2`, ..., to a common size or set of axes, chosen as the span of axes
 enclosing all of the input arrays.
+
 The padding is applied to both directions, which means original array located at the center
 the padded result. Use [`paddedviews`](@ref) if only one direction need to be padded.
+
 The axes of original array `A` will be preserved in the padded result `Ap`, hence it's true
 that `Ap[CartesianIndices(A)] == A`.
+
 ```jldoctest
 julia> using PaddedViews
+
 julia> a1 = reshape([1, 2, 3], 3, 1)
 3×1 $(Array{Int,2}):
  1
  2
  3
+
 julia> a2 = [4 5 6]
 1×3 $(Array{Int,2}):
  4  5  6
+
 julia> a1p, a2p = sym_paddedviews(-1, a1, a2);
+
 julia> a1p
 3×3 PaddedView(-1, ::$(Array{Int,2}), (1:3, 0:2)) with eltype $Int with indices 1:3×0:2:
  -1  1  -1
  -1  2  -1
  -1  3  -1
+
 julia> a2p
 3×3 PaddedView(-1, ::$(Array{Int,2}), (0:2, 1:3)) with eltype $Int with indices 0:2×1:3:
  -1  -1  -1
   4   5   6
  -1  -1  -1
+
 julia> a1p[CartesianIndices(a1)]
 3×1 $(Array{Int,2}):
  1
